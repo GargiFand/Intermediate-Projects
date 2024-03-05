06f1b503dc7f248fa48dd9bdf4bc0c423386b80d
Gargi Mangesh Fand <142970491+GargiFand@users.noreply.github.com>
2024-03-05 12:29:41 +0530

    Add files via upload

---
+++ techeverri-cli-main/techeverri-cli-main/README.md
@@ -0,0 +1,39 @@
+![GitHub](https://img.shields.io/github/license/techeverri/techeverri-cli?color=blue) [![CI](https://github.com/techeverri/techeverri-cli/actions/workflows/ci.yml/badge.svg)](https://github.com/techeverri/techeverri-cli/actions/workflows/ci.yml) [![GitHub version](https://badge.fury.io/gh/techeverri%2Ftecheverri-cli.svg)](https://badge.fury.io/gh/techeverri%2Ftecheverri-cli) [![npm version](https://badge.fury.io/js/techeverri.svg)](https://badge.fury.io/js/techeverri)
+
+### It's me Tomas! 👋
+
+_Inspired by [bitandbang](https://github.com/bnb/bitandbang)'s card._
+
+## Usage
+
+### Prerequisites
+
+- Requires [Node.js](https://nodejs.org/) (v14 or newer)
+
+### npm
+
+To see my `npm` card, run the following command:
+
+```sh
+npx techeverri
+```
+
+## Development
+
+### Prerequisites
+
+- Requires [Node.js](https://nodejs.org/) (v16 or newer)
+
+### Commands
+
+| Command          | Description                       |
+| ---------------- | --------------------------------- |
+| `npm run print`  | Print the card                    |
+| `npm run build`  | Build and print the card          |
+| `npm run test`   | Run smoke tests (Jest)            |
+| `npm run format` | Format all files (Prettier)       |
+| `npm run lint`   | Run static code analysis (ESLint) |
+
+## Screenshots
+
+![image](./screenshot.png)
---
+++ techeverri-cli-main/techeverri-cli-main/build.js
@@ -0,0 +1,62 @@
+import fs from "fs";
+import path from "path";
+import url from "url";
+import chalk from "chalk";
+import boxen from "boxen";
+
+// https://github.com/chalk/chalk#chalklevel
+chalk.level = 3;
+
+const colombia = chalk.yellow("Colo") + chalk.blue("mb") + chalk.red("ia");
+
+const employer = chalk.green.italic("Shopify");
+
+const basics = {
+  name: "Tomas Echeverri",
+  handle: "techeverri",
+  location: `Made in ${colombia}. Living in Sweden.`,
+  work: `Software Developer at ${employer}`,
+};
+
+const links = {
+  github: `https://github.com/${chalk.green("techeverri")}`,
+  linkedin: `https://www.linkedin.com/in/${chalk.blue("tomechval")}`,
+  npm: `https://www.npmjs.com/${chalk.red("~techeverri")}`,
+  twitter: `https://twitter.com/${chalk.cyan("TomasEcheverri")}`,
+  website: "https://techeverri.dev",
+  mastodon: `https://mastodon.social/${chalk.magenta("@techeverri")}`,
+};
+
+const text = `
+${chalk.white(basics.name)} / ${chalk.white(basics.handle)}
+${chalk.gray.italic(basics.location)}
+
+     ${chalk.white.bold("Work:")} ${chalk.white(basics.work)}
+  ${chalk.white.bold("Twitter:")} ${chalk.gray(links.twitter)}
+ ${chalk.white.bold("Mastodon:")} ${chalk.gray(links.mastodon)}
+   ${chalk.white.bold("GitHub:")} ${chalk.gray(links.github)}
+      ${chalk.white.bold("npm:")} ${chalk.gray(links.npm)}
+ ${chalk.white.bold("LinkedIn:")} ${chalk.gray(links.linkedin)}
+  ${chalk.white.bold("Website:")} ${chalk.magenta(links.website)}
+
+     ${chalk.white.bold("Card:")} npx techeverri
+`;
+
+const boxenOptions = {
+  margin: 1,
+  padding: 1,
+  borderStyle: "single",
+};
+
+const data = chalk.green(boxen(text.trim(), boxenOptions));
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const file = path.join(__dirname, "bin", "card");
+
+const writeFileOptions = {
+  encoding: "utf8",
+  flag: "w",
+};
+
+fs.writeFileSync(file, data, writeFileOptions);
